f97d6e9c2220e23017b4cafd4b518d24a1c90c82
rahulaes17 <rahulaes1704@gmail.com>
2026-03-04 12:24:04 +0530

    Callbacks

---
+++ Callbacks/index.js
@@ -0,0 +1,28 @@
+//Callback function
+function greeting(user){
+    console.log("Helloooo " + user)
+}
+
+function callback(call){
+    let userName = "Rahul"
+    call(userName);
+}
+
+callback(greeting);
+
+
+
+
+
+//Callbacks in Asynchronous Code
+setTimeout(()=>{
+    console.log("This is the enddddd............")
+},2000)
+
+//OR
+
+const func = () =>{
+    console.log("This is not the enddddd.........")
+}
+
+setTimeout(func , 1000)
