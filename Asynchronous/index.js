14a2d273086dd4f38ff7445880691696824d8383
rahulaes17 <rahulaes1704@gmail.com>
2026-03-04 12:23:20 +0530

    Async

---
+++ Asynchronous/index.js
@@ -0,0 +1,11 @@
+console.log("A");
+console.log("B");
+console.log("C");
+
+
+
+
+//ASYNCHRONOUS
+console.log("Code Starts")
+setTimeout(() => {console.log("Waiting is Over"),2000})
+console.log("Waiting");
