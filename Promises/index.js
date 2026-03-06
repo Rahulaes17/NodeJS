fd8f5e71d702f4c47c4abdbac3d8c254cc5a7df2
rahulaes17 <rahulaes1704@gmail.com>
2026-03-07 00:11:52 +0530

    Promises error fix

--- Promises/index.js
+++ Promises/index.js
@@ -1,53 +1,95 @@
-//callback hell
+function waitTwoSeconds() {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            resolve("Done waiting");
+        }, 2000);
+    });
+}
 
-//doTask1(function (result1) {
-//  doTask2(result1, function (result2) {
-//  doTask3(result2, function (result3) {
-//  console.log("All tasks done");
-// });
-// });
-// });
+console.log("Start");
 
-
-
-//Promise Syntax
-    const promise = new Promise(function (resolve, reject) {
-    // Do some work...
-    // Call resolve(result) if successful
-    // Call reject(error) if there’s an error
+waitTwoSeconds()
+    .then(function (message) {
+        console.log(message);
+    })
+    .catch(function (error) {
+        console.log("Something went wrong");
     });
 
-//instead
-doTask1()
-.then(function (result1) {
-return doTask2(result1);
-})
-.then(function (result2) {
-return doTask3(result2);
-})
-.then(function (result3) {
- console.log("All tasks done");
-})
-.catch(function (error) {
- console.log("Something failed", error);
-});
-
-
-
-
-function waitTwoSeconds() {
-return new Promise(function (resolve, reject) {
- setTimeout(function () {
- resolve("Done waiting");
-}, 2000);
-});
-}
-console.log("Start");
-waitTwoSeconds()
-.then(function (message) {
- console.log(message); // "Done waiting"
-})
-.catch(function (error) {
- console.log("Something went wrong");
-});
 console.log("End");
+
+
+
+function doTask1(callback) {
+    setTimeout(function () {
+        console.log("Task 1 done");
+        callback("Result 1");
+    }, 1000);
+}
+
+function doTask2(data, callback) {
+    setTimeout(function () {
+        console.log("Task 2 done with", data);
+        callback("Result 2");
+    }, 1000);
+}
+
+function doTask3(data, callback) {
+    setTimeout(function () {
+        console.log("Task 3 done with", data);
+        callback("Result 3");
+    }, 1000);
+}
+
+// Callback Hell
+doTask1(function (result1) {
+    doTask2(result1, function (result2) {
+        doTask3(result2, function (result3) {
+            console.log("All tasks done");
+        });
+    });
+});
+
+
+
+//promise chaining
+function doTask1() {
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            console.log("Task 1 done");
+            resolve("Result 1");
+        }, 1000);
+    });
+}
+
+function doTask2(data) {
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            console.log("Task 2 done with", data);
+            resolve("Result 2");
+        }, 1000);
+    });
+}
+
+function doTask3(data) {
+    return new Promise(function (resolve) {
+        setTimeout(function () {
+            console.log("Task 3 done with", data);
+            resolve("Result 3");
+        }, 1000);
+    });
+}
+
+doTask1()
+    .then(function (result1) {
+        return doTask2(result1);
+    })
+    .then(function (result2) {
+        return doTask3(result2);
+    })
+    .then(function (result3) {
+        console.log("All tasks done");
+    })
+    .catch(function (error) {
+        console.log("Something failed", error);
+    });
