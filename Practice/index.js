53a47bcfce7d519fc76a6e089e8178396892687b
rahulaes17 <rahulaes1704@gmail.com>
2026-03-13 23:37:25 +0530

    Overall Practice

---
+++ Practice/index.js
@@ -0,0 +1,63 @@
+// import modules
+const fs = require("fs");
+const http = require("http");
+
+
+// 1. Callback example
+function add(a, b, callback) {
+    let result = a + b;
+    callback(result);
+}
+
+add(5, 10, function (result) {
+    console.log("Sum is:", result);
+});
+
+
+// 2. File System (write file)
+fs.writeFile("sample.txt", "Hello from NodeJS", function (err) {
+    if (err) {
+        console.log("Error writing file");
+    } else {
+        console.log("File created successfully");
+    }
+});
+
+
+// 3. File System (read file)
+fs.readFile("sample.txt", "utf8", function (err, data) {
+    if (err) {
+        console.log("Error reading file");
+    } else {
+        console.log("File content:", data);
+    }
+});
+
+
+// 4. Async/Await with API
+async function getUsers() {
+    try {
+        let response = await fetch("https://jsonplaceholder.typicode.com/users");
+        let users = await response.json();
+
+        console.log("First User:", users[0].name);
+    } catch (error) {
+        console.log("API Error:", error);
+    }
+}
+
+getUsers();
+
+
+// 5. HTTP Server
+const server = http.createServer(function (req, res) {
+
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("Hello from Node Server");
+    res.end();
+
+});
+
+server.listen(3000, function () {
+    console.log("Server running at http://localhost:3000");
+});
