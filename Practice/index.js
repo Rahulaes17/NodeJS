59f42630eb4e733a08db19406ae08cae2c6815f7
rahulaes17 <rahulaes1704@gmail.com>
2026-03-15 22:28:53 +0530

    structure improved of the practice

--- Practice/index.js
+++ Practice/index.js
@@ -1,21 +1,20 @@
-// import modules
 const fs = require("fs");
 const http = require("http");
+const path = require("path");
 
+const filePath = path.join(__dirname, "sample.txt");
+console.log("File path:", filePath);
 
-// 1. Callback example
 function add(a, b, callback) {
     let result = a + b;
     callback(result);
 }
 
-add(5, 10, function (result) {
+add(5, 10, function(result) {
     console.log("Sum is:", result);
 });
 
-
-// 2. File System (write file)
-fs.writeFile("sample.txt", "Hello from NodeJS", function (err) {
+fs.writeFile(filePath, "Hello from NodeJS", function(err) {
     if (err) {
         console.log("Error writing file");
     } else {
@@ -23,24 +22,36 @@ fs.writeFile("sample.txt", "Hello from NodeJS", function (err) {
     }
 });
 
-
-// 3. File System (read file)
-fs.readFile("sample.txt", "utf8", function (err, data) {
+fs.appendFile(filePath, "\nThis line was appended", function(err) {
     if (err) {
-        console.log("Error reading file");
+        console.log("Append failed");
     } else {
-        console.log("File content:", data);
+        console.log("Content appended");
     }
 });
 
+fs.readFile(filePath, "utf8", function(err, data) {
+    if (err) {
+        console.log("Error reading file");
+    } else {
+        console.log("File content:\n", data);
+    }
+});
 
-// 4. Async/Await with API
+console.log("Command line arguments:", process.argv);
+
 async function getUsers() {
     try {
-        let response = await fetch("https://jsonplaceholder.typicode.com/users");
-        let users = await response.json();
+        console.log("Fetching users...");
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
 
-        console.log("First User:", users[0].name);
+        if (!response.ok) {
+            throw new Error("API request failed");
+        }
+
+        const users = await response.json();
+        console.log("First user:", users[0].name);
+
     } catch (error) {
         console.log("API Error:", error);
     }
@@ -48,16 +59,32 @@ async function getUsers() {
 
 getUsers();
 
+const server = http.createServer(function(req, res) {
 
-// 5. HTTP Server
-const server = http.createServer(function (req, res) {
+    console.log(req.method, req.url);
 
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.write("Hello from Node Server");
+
+    if (req.url === "/") {
+        res.write("Home Page");
+    }
+
+    else if (req.url === "/about") {
+        res.write("About Page");
+    }
+
+    else if (req.url === "/users") {
+        res.write("Users Page");
+    }
+
+    else {
+        res.write("404 Page Not Found");
+    }
+
     res.end();
 
 });
 
-server.listen(3000, function () {
+server.listen(3000, function() {
     console.log("Server running at http://localhost:3000");
 });
