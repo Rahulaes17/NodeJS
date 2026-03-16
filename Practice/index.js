d7ccb06a2a3990dda0676e1b8aa7da36841d97ac
Rahul Joshi <rahulaes1704@gmail.com>
2026-03-16 23:56:20 +0530

    improved file handling order, removed duplicate append, added EventEmitter, timers, JSON response, and system info

--- Practice/index.js
+++ Practice/index.js
@@ -1,9 +1,13 @@
 const fs = require("fs");
 const http = require("http");
 const path = require("path");
+const os = require("os");
+const EventEmitter = require("events");
+
+console.log("Directory:", __dirname);
+console.log("File:", __filename);
 
 const filePath = path.join(__dirname, "sample.txt");
-console.log("File path:", filePath);
 
 function add(a, b, callback) {
     let result = a + b;
@@ -15,27 +19,21 @@ add(5, 10, function(result) {
 });
 
 fs.writeFile(filePath, "Hello from NodeJS", function(err) {
-    if (err) {
-        console.log("Error writing file");
-    } else {
-        console.log("File created successfully");
-    }
-});
+    if (err) return console.log("Error writing file");
 
-fs.appendFile(filePath, "\nThis line was appended", function(err) {
-    if (err) {
-        console.log("Append failed");
-    } else {
+    console.log("File created");
+
+    fs.appendFile(filePath, "\nThis line was appended", function(err) {
+        if (err) return console.log("Append failed");
+
         console.log("Content appended");
-    }
-});
 
-fs.readFile(filePath, "utf8", function(err, data) {
-    if (err) {
-        console.log("Error reading file");
-    } else {
-        console.log("File content:\n", data);
-    }
+        fs.readFile(filePath, "utf8", function(err, data) {
+            if (err) return console.log("Error reading file");
+
+            console.log("File content:\n", data);
+        });
+    });
 });
 
 console.log("Command line arguments:", process.argv);
@@ -43,6 +41,7 @@ console.log("Command line arguments:", process.argv);
 async function getUsers() {
     try {
         console.log("Fetching users...");
+
         const response = await fetch("https://jsonplaceholder.typicode.com/users");
 
         if (!response.ok) {
@@ -50,6 +49,7 @@ async function getUsers() {
         }
 
         const users = await response.json();
+
         console.log("First user:", users[0].name);
 
     } catch (error) {
@@ -59,68 +59,69 @@ async function getUsers() {
 
 getUsers();
 
+function multiply(a, b) {
+    return new Promise((resolve, reject) => {
+        let result = a * b;
+        resolve(result);
+    });
+}
+
+multiply(4, 5)
+.then(result => console.log("Multiply Result:", result))
+.catch(err => console.log(err));
+
+const emitter = new EventEmitter();
+
+emitter.on("greet", function(name) {
+    console.log("Hello", name);
+});
+
+emitter.emit("greet", "Rahul");
+
+setTimeout(() => {
+    console.log("Executed after 2 seconds");
+}, 2000);
+
+setInterval(() => {
+    console.log("Running interval task");
+}, 5000);
+
+console.log("Platform:", os.platform());
+console.log("CPU Architecture:", os.arch());
+console.log("Free Memory:", os.freemem());
+
 const server = http.createServer(function(req, res) {
 
     console.log(req.method, req.url);
 
-    res.writeHead(200, { "Content-Type": "text/plain" });
-
     if (req.url === "/") {
-        res.write("Home Page");
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("Home Page");
     }
 
     else if (req.url === "/about") {
-        res.write("About Page");
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end("About Page");
     }
 
     else if (req.url === "/users") {
-        res.write("Users Page");
+        res.writeHead(200, { "Content-Type": "application/json" });
+
+        const users = [
+            { name: "Rahul", age: 20 },
+            { name: "Rohit", age: 22 }
+        ];
+
+        res.end(JSON.stringify(users));
     }
 
     else {
-        res.write("404 Page Not Found");
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("404 Page Not Found");
     }
-
-    res.end();
 
 });
 
 server.listen(3000, function() {
     console.log("Server running at http://localhost:3000");
 });
-
-
-
-function multiply(a, b) {
-    return new Promise((resolve, reject) => {
-        let result = a * b;
-
-        if (result) {
-            resolve(result);
-        } else {
-            reject("Error in multiplication");
-        }
-    });
-}
-
-multiply(4, 5)
-    .then(result => console.log("Multiply Result:", result))
-    .catch(err => console.log(err));
-
-
-
-fs.appendFile("sample.txt", "\nAppended text from NodeJS", function (err) {
-    if (err) {
-        console.log("Error appending file");
-    } else {
-        console.log("Content appended");
-    }
-});
-
-const os = require("os");
-
-console.log("Platform:", os.platform());
-console.log("CPU Architecture:", os.arch());
-console.log("Free Memory:", os.freemem());
-
-
